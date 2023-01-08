0be3181178142071a01ff9e07d877afc9a09ca1a
Andi Nawfal <nawfaldzikra1611@gmail.com>
2023-01-08 15:22:31 +0700

    upgrade dashboard, logging admin

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index(){
+        $supplier = DB::table('supplier')->count();
+        return view('partials.sidebar', compact('supplier'));
+    }
 }
--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -16,7 +16,12 @@ class DashboardController extends Controller
         $ruangan = DB::table('ruangan')->count();
         $pengajuan_bb = DB::table('pengajuan_bb')->where('status_approval', 'setuju')->count();
         $pemutihan = DB::table('pemutihan')->where('approve_penonaktifan', 'setuju')->count();
-        $latest_detail_barang = DB::table('detail_barang_jenis_barang')->paginate(10);
+        $latest_detail_barang = DB::table('detail_barang_jenis_barang')->paginate(5);
+        $latest_logging = DB::table('log')->orderByDesc('id_log')->paginate(5);
+        $bb_outstanding = DB::table('pengajuan_bb')->where('status_pembelian', NULL)->paginate(5);
+
+        // $id_perbaikan_rusak = DB::select('SELECT kode_barang FROM perbaikan WHERE approve_perbaikan = "rusak"'
+        // dd($cek_pemutihan);
         // dd($latest_detail_barang);
 
         return view('partials.dashboard', compact(
@@ -25,7 +30,9 @@ class DashboardController extends Controller
             'pengajuan_bb',
             'pemutihan',
             'ruangan',
-            'latest_detail_barang'
+            'latest_detail_barang',
+            'latest_logging',
+            'bb_outstanding'
         ));
     }
 }
