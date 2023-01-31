b53c0b6e895abe94ed5a30a45bd81c3e6a5a632f
Andi Nawfal <nawfaldzikra1611@gmail.com>
2023-01-31 08:22:37 +0700

    middleware routes

--- config/laravolt/avatar.php
+++ config/laravolt/avatar.php
@@ -5,7 +5,7 @@
  */
 return [
 
-    /*
+  /*
     |--------------------------------------------------------------------------
     | Image Driver
     |--------------------------------------------------------------------------
@@ -17,46 +17,97 @@ return [
     | Supported: "gd", "imagick"
     |
     */
-    'driver' => env('IMAGE_DRIVER', 'gd'),
+  'driver' => env('IMAGE_DRIVER', 'gd'),
 
-    // Initial generator class
-    'generator' => \Laravolt\Avatar\Generator\DefaultGenerator::class,
+  // Initial generator class
+  'generator' => \Laravolt\Avatar\Generator\DefaultGenerator::class,
 
-    // Whether all characters supplied must be replaced with their closest ASCII counterparts
-    'ascii' => false,
+  // Whether all characters supplied must be replaced with their closest ASCII counterparts
+  'ascii' => false,
 
-    // Image shape: circle or square
-    'shape' => 'circle',
+  // Image shape: circle or square
+  'shape' => 'circle',
 
-    // Image width, in pixel
-    'width' => 100,
+  // Image width, in pixel
+  'width' => 300,
 
-    // Image height, in pixel
-    'height' => 100,
+  // Image height, in pixel
+  'height' => 300,
 
-    // Number of characters used as initials. If name consists of single word, the first N character will be used
-    'chars' => 2,
+  // Number of characters used as initials. If name consists of single word, the first N character will be used
+  'chars' => 2,
 
-    // font size
-    'fontSize' => 48,
+  // font size
+  'fontSize' => 144,
 
-    // convert initial letter in uppercase
-    'uppercase' => true,
+  // convert initial letter in uppercase
+  'uppercase' => false,
 
-    // Right to Left (RTL)
-    'rtl' => false,
+  // Right to Left (RTL)
+  'rtl' => false,
 
-    // Fonts used to render text.
-    // If contains more than one fonts, randomly selected based on name supplied
-    'fonts' => [__DIR__ . '/../fonts/OpenSans-Bold.ttf', __DIR__ . '/../fonts/rockwell.ttf'],
+  // Fonts used to render text.
+  // If contains more than one fonts, randomly selected based on name supplied
+  'fonts' => [__DIR__ . '/../fonts/OpenSans-Bold.ttf', __DIR__ . '/../fonts/rockwell.ttf'],
 
-    // List of foreground colors to be used, randomly selected based on name supplied
-    'foregrounds' => [
-        '#FFFFFF',
+  // List of foreground colors to be used, randomly selected based on name supplied
+  'foregrounds' => [
+    '#FFFFFF',
+  ],
+
+  // List of background colors to be used, randomly selected based on name supplied
+  'backgrounds' => [
+    '#f44336',
+    '#E91E63',
+    '#9C27B0',
+    '#673AB7',
+    '#3F51B5',
+    '#2196F3',
+    '#03A9F4',
+    '#00BCD4',
+    '#009688',
+    '#4CAF50',
+    '#8BC34A',
+    '#CDDC39',
+    '#FFC107',
+    '#FF9800',
+    '#FF5722',
+  ],
+
+  'border' => [
+    'size' => 1,
+
+    // border color, available value are:
+    // 'foreground' (same as foreground color)
+    // 'background' (same as background color)
+    // or any valid hex ('#aabbcc')
+    'color' => 'background',
+
+    // border radius, currently only work for SVG
+    'radius' => 0,
+  ],
+
+  // List of theme name to be used when rendering avatar
+  // Possible values are:
+  // 1. Theme name as string: 'colorful'
+  // 2. Or array of string name: ['grayscale-light', 'grayscale-dark']
+  // 3. Or wildcard "*" to use all defined themes
+  'theme' => ['colorful'],
+
+  // Predefined themes
+  // Available theme attributes are:
+  // shape, chars, backgrounds, foregrounds, fonts, fontSize, width, height, ascii, uppercase, and border.
+  'themes' => [
+    'grayscale-light' => [
+      'backgrounds' => ['#edf2f7', '#e2e8f0', '#cbd5e0'],
+      'foregrounds' => ['#a0aec0'],
     ],
-
-    // List of background colors to be used, randomly selected based on name supplied
-    'backgrounds' => [
+    'grayscale-dark' => [
+      'backgrounds' => ['#2d3748', '#4a5568', '#718096'],
+      'foregrounds' => ['#e2e8f0'],
+    ],
+    'colorful' => [
+      'backgrounds' => [
         '#f44336',
         '#E91E63',
         '#9C27B0',
@@ -72,80 +123,29 @@ return [
         '#FFC107',
         '#FF9800',
         '#FF5722',
+      ],
+      'foregrounds' => ['#FFFFFF'],
     ],
-
-    'border' => [
-        'size' => 1,
-
-        // border color, available value are:
-        // 'foreground' (same as foreground color)
-        // 'background' (same as background color)
-        // or any valid hex ('#aabbcc')
-        'color' => 'background',
-
-        // border radius, currently only work for SVG
-        'radius' => 0,
+    'pastel' => [
+      'backgrounds' => [
+        '#ef9a9a',
+        '#F48FB1',
+        '#CE93D8',
+        '#B39DDB',
+        '#9FA8DA',
+        '#90CAF9',
+        '#81D4FA',
+        '#80DEEA',
+        '#80CBC4',
+        '#A5D6A7',
+        '#E6EE9C',
+        '#FFAB91',
+        '#FFCCBC',
+        '#D7CCC8',
+      ],
+      'foregrounds' => [
+        '#FFF',
+      ],
     ],
-
-    // List of theme name to be used when rendering avatar
-    // Possible values are:
-    // 1. Theme name as string: 'colorful'
-    // 2. Or array of string name: ['grayscale-light', 'grayscale-dark']
-    // 3. Or wildcard "*" to use all defined themes
-    'theme' => ['colorful'],
-
-    // Predefined themes
-    // Available theme attributes are:
-    // shape, chars, backgrounds, foregrounds, fonts, fontSize, width, height, ascii, uppercase, and border.
-    'themes' => [
-        'grayscale-light' => [
-            'backgrounds' => ['#edf2f7', '#e2e8f0', '#cbd5e0'],
-            'foregrounds' => ['#a0aec0'],
-        ],
-        'grayscale-dark' => [
-            'backgrounds' => ['#2d3748', '#4a5568', '#718096'],
-            'foregrounds' => ['#e2e8f0'],
-        ],
-        'colorful' => [
-            'backgrounds' => [
-                '#f44336',
-                '#E91E63',
-                '#9C27B0',
-                '#673AB7',
-                '#3F51B5',
-                '#2196F3',
-                '#03A9F4',
-                '#00BCD4',
-                '#009688',
-                '#4CAF50',
-                '#8BC34A',
-                '#CDDC39',
-                '#FFC107',
-                '#FF9800',
-                '#FF5722',
-            ],
-            'foregrounds' => ['#FFFFFF'],
-        ],
-        'pastel' => [
-            'backgrounds' => [
-                '#ef9a9a',
-                '#F48FB1',
-                '#CE93D8',
-                '#B39DDB',
-                '#9FA8DA',
-                '#90CAF9',
-                '#81D4FA',
-                '#80DEEA',
-                '#80CBC4',
-                '#A5D6A7',
-                '#E6EE9C',
-                '#FFAB91',
-                '#FFCCBC',
-                '#D7CCC8',
-            ],
-            'foregrounds' => [
-                '#FFF',
-            ],
-        ],
-    ],
+  ],
 ];
--- routes/web.php
+++ routes/web.php
@@ -40,16 +40,16 @@ Route::get('/', function () {
 
 Route::get('/dashboard',[DashboardController::class,'dashboard']);
 
-Route::get('/barang',[ BarangController::class,'index']);
-Route::get('/barang/search',[ BarangController::class,'search']);
-Route::get('/barang/detail/{id}',[ BarangController::class,'detail']);
-Route::get('/barang/detail/spesifik/{id}',[ BarangController::class,'spesifik']);
-Route::get('/barang/detail/search/{id}',[ BarangController::class,'searchdetail']);
-Route::post('/barang/detail/update/{id}',[ BarangController::class,'update']);
+Route::get('/barang',[ BarangController::class,'index'])->middleware('auth');
+Route::get('/barang/search',[ BarangController::class,'search'])->middleware('auth');
+Route::get('/barang/detail/{id}',[ BarangController::class,'detail'])->middleware('auth');
+Route::get('/barang/detail/spesifik/{id}',[ BarangController::class,'spesifik'])->middleware('auth');
+Route::get('/barang/detail/search/{id}',[ BarangController::class,'searchdetail'])->middleware('auth');
+Route::post('/barang/detail/update/{id}',[ BarangController::class,'update'])->middleware('auth');
 
 
-Route::get('/levelUser/tambah',[LevelUserController::class,'formTambah']);
-Route::post('/levelUser/simpan',[LevelUserController::class,'simpan']);
+Route::get('/levelUser/tambah',[LevelUserController::class,'formTambah'])->middleware('auth');
+Route::post('/levelUser/simpan',[LevelUserController::class,'simpan'])->middleware('auth');
 
 Route::get('login',[LoginController::class,'login'])->name('login');
 Route::post('/login',[LoginController::class,'authenticate']);
@@ -58,125 +58,125 @@ Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 
 Route::group(['middleware' => ['auth','level:admin']], function (){
-    Route::get('/log',[ LoggingController::class,'index']);
-    Route::get('/log/search',[ LoggingController::class,'search']);
+    Route::get('/log',[ LoggingController::class,'index'])->middleware('auth');
+    Route::get('/log/search',[ LoggingController::class,'search'])->middleware('auth');
 });
 
 //Route Group untuk menggabungkan banyaknya middleware
 Route::group(['middleware' => ['auth','level:admin,manajemen']], function (){
-    // Route::get('/levelUser',[LevelUserController::class,'index']);
-    Route::get('/approval/BB',[ ApprovalController::class,'indexBarangBaru']);
-    Route::get('/approval/BB/search',[ ApprovalController::class,'searchindexBarangBaru']);
-    Route::get('/approval/BB/detail/{id}',[ApprovalController::class,'detailBarangBaru']);
-    Route::get('/approval/BB/setuju/{id}',[ApprovalController::class,'statusSetujuBarangBaru']);
-    Route::get('/approval/BB/tidaksetuju/{id}',[ApprovalController::class,'statusTidakSetujuBarangBaru']);
+    // Route::get('/levelUser',[LevelUserController::class,'index'])->middleware('auth');
+    Route::get('/approval/BB',[ ApprovalController::class,'indexBarangBaru'])->middleware('auth');
+    Route::get('/approval/BB/search',[ ApprovalController::class,'searchindexBarangBaru'])->middleware('auth');
+    Route::get('/approval/BB/detail/{id}',[ApprovalController::class,'detailBarangBaru'])->middleware('auth');
+    Route::get('/approval/BB/setuju/{id}',[ApprovalController::class,'statusSetujuBarangBaru'])->middleware('auth');
+    Route::get('/approval/BB/tidaksetuju/{id}',[ApprovalController::class,'statusTidakSetujuBarangBaru'])->middleware('auth');
 
-    Route::get('/approval/PB',[ ApprovalController::class,'indexPerbaikan']);
-    Route::get('/approval/PB/search',[ ApprovalController::class,'searchindexPerbaikan']);
-    Route::get('/approval/PB/detail/{id}',[ApprovalController::class,'detailPerbaikan']);
-    Route::get('/approval/PB/setuju/{id}',[ApprovalController::class,'statusSetujuPerbaikan']);
-    Route::get('/approval/PB/tidaksetuju/{id}/{kode}',[ApprovalController::class,'statusTidakSetujuPerbaikan']);
+    Route::get('/approval/PB',[ ApprovalController::class,'indexPerbaikan'])->middleware('auth');
+    Route::get('/approval/PB/search',[ ApprovalController::class,'searchindexPerbaikan'])->middleware('auth');
+    Route::get('/approval/PB/detail/{id}',[ApprovalController::class,'detailPerbaikan'])->middleware('auth');
+    Route::get('/approval/PB/setuju/{id}',[ApprovalController::class,'statusSetujuPerbaikan'])->middleware('auth');
+    Route::get('/approval/PB/tidaksetuju/{id}/{kode}',[ApprovalController::class,'statusTidakSetujuPerbaikan'])->middleware('auth');
 
-    Route::get('/approval/pemutihan',[ ApprovalController::class,'indexPemutihan']);
-    Route::get('/approval/pemutihan/search',[ ApprovalController::class,'searchindexPemutihan']);
-    Route::get('/approval/pemutihan/detail/{id}',[ApprovalController::class,'detailPemutihan']);
-    Route::get('/approval/pemutihan/setuju/{id}/{kode}',[ApprovalController::class,'statusSetujuPemutihan']);
-    Route::get('/approval/pemutihan/tidaksetuju/{id}',[ApprovalController::class,'statusTidakSetujuPemutihan']);
+    Route::get('/approval/pemutihan',[ ApprovalController::class,'indexPemutihan'])->middleware('auth');
+    Route::get('/approval/pemutihan/search',[ ApprovalController::class,'searchindexPemutihan'])->middleware('auth');
+    Route::get('/approval/pemutihan/detail/{id}',[ApprovalController::class,'detailPemutihan'])->middleware('auth');
+    Route::get('/approval/pemutihan/setuju/{id}/{kode}',[ApprovalController::class,'statusSetujuPemutihan'])->middleware('auth');
+    Route::get('/approval/pemutihan/tidaksetuju/{id}',[ApprovalController::class,'statusTidakSetujuPemutihan'])->middleware('auth');
 });
 
-Route::get('/User',[ UserController::class,'index']);
-Route::get('/User/tambah',[ UserController::class,'formTambah']);
-Route::post('/User/simpan',[ UserController::class,'simpan']);
-Route::get('/User/edit/{id}',[UserController::class,'edit']);
-Route::post('/User/edit/editsimpan',[ UserController::class,'editsimpan']);
-Route::get('/User/hapus/{id}',[UserController::class,'hapus']);
+Route::get('/User',[ UserController::class,'index'])->middleware('auth');
+Route::get('/User/tambah',[ UserController::class,'formTambah'])->middleware('auth');
+Route::post('/User/simpan',[ UserController::class,'simpan'])->middleware('auth');
+Route::get('/User/edit/{id}',[UserController::class,'edit'])->middleware('auth');
+Route::post('/User/edit/editsimpan',[ UserController::class,'editsimpan'])->middleware('auth');
+Route::get('/User/hapus/{id}',[UserController::class,'hapus'])->middleware('auth');
 
-Route::get('/User/kaprog', [KaprogController::class, 'index']);
+Route::get('/User/kaprog', [KaprogController::class, 'index'])->middleware('auth');
 
 // Route::group(['middleware' => ['auth','level:manajemen']], function (){
 
 // });
-Route::get('/barangMasuk',[ BarangMasukController::class,'index']);
-Route::get('/barangMasuk/searchPengajuan',[ BarangMasukController::class,'searchPengajuan']);
-Route::get('/barangMasuk/searchBarangMasuk',[ BarangMasukController::class,'searchBarangMasuk']);
-Route::get('/barangMasuk/tambah/{id}',[ BarangMasukController::class,'formTambah']);
-Route::post('/barangMasuk/tambah/simpan',[ BarangMasukController::class,'store']);
+Route::get('/barangMasuk',[ BarangMasukController::class,'index'])->middleware('auth');
+Route::get('/barangMasuk/searchPengajuan',[ BarangMasukController::class,'searchPengajuan'])->middleware('auth');
+Route::get('/barangMasuk/searchBarangMasuk',[ BarangMasukController::class,'searchBarangMasuk'])->middleware('auth');
+Route::get('/barangMasuk/tambah/{id}',[ BarangMasukController::class,'formTambah'])->middleware('auth');
+Route::post('/barangMasuk/tambah/simpan',[ BarangMasukController::class,'store'])->middleware('auth');
 
-Route::get('/supplier',[ SupplierController::class,'index']);
-Route::get('/supplier/search',[ SupplierController::class,'search']);
-Route::get('/supplier/tambah',[ SupplierController::class,'formTambah']);
-Route::post('/supplier/simpan',[ SupplierController::class,'store']);
-Route::get('/supplier/edit/{id}',[SupplierController::class,'edit']);
-Route::post('/supplier/editsimpan',[ SupplierController::class,'update']);
-Route::get('/supplier/hapus/{id}',[SupplierController::class,'hapus']);
+Route::get('/supplier',[ SupplierController::class,'index'])->middleware('auth');
+Route::get('/supplier/search',[ SupplierController::class,'search'])->middleware('auth');
+Route::get('/supplier/tambah',[ SupplierController::class,'formTambah'])->middleware('auth');
+Route::post('/supplier/simpan',[ SupplierController::class,'store'])->middleware('auth');
+Route::get('/supplier/edit/{id}',[SupplierController::class,'edit'])->middleware('auth');
+Route::post('/supplier/editsimpan',[ SupplierController::class,'update'])->middleware('auth');
+Route::get('/supplier/hapus/{id}',[SupplierController::class,'hapus'])->middleware('auth');
 
-Route::get('/pengguna',[ PenggunaController::class,'index']);
-Route::get('/pengguna/search',[ PenggunaController::class,'search']);
-Route::get('/pengguna/tambah',[ PenggunaController::class,'formTambah']);
-Route::post('/pengguna/simpan',[ PenggunaController::class,'store']);
-Route::get('/pengguna/edit/{id}',[PenggunaController::class,'edit']);
-Route::post('/pengguna/edit/editsimpan',[ PenggunaController::class,'editsimpan']);
-Route::get('/pengguna/hapus/{id}',[PenggunaController::class,'hapus']);
-
-
-
-Route::get('/pengajuan/BB',[ PengajuanBBController::class,'index']);
-Route::get('/pengajuan/BB/search',[ PengajuanBBController::class,'search']);
-// Route::get('/pengajuan/BB/tambah',[ PengajuanBBController::class,'formTambah']);
-Route::post('/pengajuan/BB/tambah',[ PengajuanBBController::class,'store']);
-// Route::get('/pengajuan/BB/edit/{id}',[PengajuanBBController::class,'edit']);
-Route::post('/pengajuan/BB/update/{id}',[ PengajuanBBController::class,'update']);
-Route::get('/pengajuan/BB/hapus/{id}',[PengajuanBBController::class,'hapus']);
-Route::get('/pengajuan/BB/detail/{id}',[PengajuanBBController::class,'detail']);
-
-Route::get('/pengajuan/PB',[ PerbaikanController::class,'index']);
-Route::get('/pengajuan/PB/search',[ PerbaikanController::class,'search']);
-Route::get('/pengajuan/PB/pilihBarang',[ PerbaikanController::class,'pilihBarang']);
-Route::get('/pengajuan/PB/pilihBarang/search',[ PerbaikanController::class,'searchpilihbarang']);
-Route::get('/pengajuan/PB/edit/{id}',[PerbaikanController::class,'edit']);
-// Route::get('/pengajuan/PB/perbaikan/{id}',[PerbaikanController::class,'perbaikan']);
-Route::post('/pengajuan/PB/perbaikan/simpanperbaikan',[PerbaikanController::class,'simpanperbaikan']);
-Route::post('/pengajuan/PB/edit/editsimpan',[ PerbaikanController::class,'editsimpan']);
-Route::get('/pengajuan/PB/hapus/{id}',[PerbaikanController::class,'hapus']);
-Route::get('/PB/selesaiPerbaikan/{id}',[PerbaikanController::class,'selesaiPerbaikan']);
-Route::post('/PB/selesaiPerbaikan/simpanSelesaiPerbaikan',[ PerbaikanController::class,'simpanSelesaiPerbaikan']);
-Route::get('/pengajuan/PB/detail/{id}',[PerbaikanController::class,'detail']);
+Route::get('/pengguna',[ PenggunaController::class,'index'])->middleware('auth');
+Route::get('/pengguna/search',[ PenggunaController::class,'search'])->middleware('auth');
+Route::get('/pengguna/tambah',[ PenggunaController::class,'formTambah'])->middleware('auth');
+Route::post('/pengguna/simpan',[ PenggunaController::class,'store'])->middleware('auth');
+Route::get('/pengguna/edit/{id}',[PenggunaController::class,'edit'])->middleware('auth');
+Route::post('/pengguna/edit/editsimpan',[ PenggunaController::class,'editsimpan'])->middleware('auth');
+Route::get('/pengguna/hapus/{id}',[PenggunaController::class,'hapus'])->middleware('auth');
 
 
-Route::get('/pemutihan',[ PemutihanController::class,'index']);
-Route::get('/pemutihan/pilihbarang',[ PemutihanController::class,'pilihbarang']);
-Route::get('/pemutihan/pilihbarang/search',[ PemutihanController::class,'pilihbarang']);
-Route::get('/pemutihan/pemutihan/{id}',[PemutihanController::class,'pemutihan']);
-Route::post('/pemutihan/pemutihan/simpanpemutihan',[PemutihanController::class,'simpanpemutihan']);
-Route::get('/pemutihan/detail/{id}',[PemutihanController::class,'detail']);
 
-Route::get('/pemutihanLangsung/pilihBarang',[ PemutihanController::class,'pilihbarangPemutihanLangsung']);
-Route::get('/pemutihanLangsung/pilihBarang/searchPLangsung',[ PemutihanController::class,'searchPLangsung']);
-Route::get('/pemutihan/pemutihanLangung/{id}',[PemutihanController::class,'pemutihanLangsung']);
-Route::post('/pemutihan/pemutihanLangsung/simpanpemutihanLangsung',[PemutihanController::class,'simpanpemutihanLangsung']);
+Route::get('/pengajuan/BB',[ PengajuanBBController::class,'index'])->middleware('auth');
+Route::get('/pengajuan/BB/search',[ PengajuanBBController::class,'search'])->middleware('auth');
+// Route::get('/pengajuan/BB/tambah',[ PengajuanBBController::class,'formTambah'])->middleware('auth');
+Route::post('/pengajuan/BB/tambah',[ PengajuanBBController::class,'store'])->middleware('auth');
+// Route::get('/pengajuan/BB/edit/{id}',[PengajuanBBController::class,'edit'])->middleware('auth');
+Route::post('/pengajuan/BB/update/{id}',[ PengajuanBBController::class,'update'])->middleware('auth');
+Route::get('/pengajuan/BB/hapus/{id}',[PengajuanBBController::class,'hapus'])->middleware('auth');
+Route::get('/pengajuan/BB/detail/{id}',[PengajuanBBController::class,'detail'])->middleware('auth');
 
-Route::get('/perawatan',[ PerawatanController::class,'index']);
-Route::get('/perawatan/search',[ PerawatanController::class,'search']);
-Route::get('/perawatan/pilihBarang',[ PerawatanController::class,'pilihbarangPerawatan']);
-Route::get('/perawatan/pilihBarang/search',[ PerawatanController::class,'searchpilihbarangPerawatan']);
-Route::get('/perawatan/tambah/{id}',[PerawatanController::class,'perawatan']);
-Route::post('/perawatan/simpanperawatan',[PerawatanController::class,'simpanperawatan']);
-Route::get('/perawatan/detail/{id}',[PerawatanController::class,'detail']);
-Route::get('/perawatan/edit/{id}',[PerawatanController::class,'edit']);
-Route::post('/perawatan/edit/editsimpan',[ PerawatanController::class,'editsimpan']);
-Route::get('/perawatan/hapus/{id}',[PerawatanController::class,'hapus']);
-
-
-Route::get('/ruangan',[ RuanganController::class,'index']);
-Route::get('/ruangan/search',[ RuanganController::class,'search']);
-Route::get('/ruangan/tambah',[ RuanganController::class,'formTambah']);
-Route::post('/ruangan/simpan',[ RuanganController::class,'store']);
-Route::get('/ruangan/edit/{id}',[RuanganController::class,'edit']);
-Route::post('/ruangan/editsimpan',[ RuanganController::class,'update']);
-Route::get('/ruangan/hapus/{id}',[RuanganController::class,'hapus']);
+Route::get('/pengajuan/PB',[ PerbaikanController::class,'index'])->middleware('auth');
+Route::get('/pengajuan/PB/search',[ PerbaikanController::class,'search'])->middleware('auth');
+Route::get('/pengajuan/PB/pilihBarang',[ PerbaikanController::class,'pilihBarang'])->middleware('auth');
+Route::get('/pengajuan/PB/pilihBarang/search',[ PerbaikanController::class,'searchpilihbarang'])->middleware('auth');
+Route::get('/pengajuan/PB/edit/{id}',[PerbaikanController::class,'edit'])->middleware('auth');
+// Route::get('/pengajuan/PB/perbaikan/{id}',[PerbaikanController::class,'perbaikan'])->middleware('auth');
+Route::post('/pengajuan/PB/perbaikan/simpanperbaikan',[PerbaikanController::class,'simpanperbaikan'])->middleware('auth');
+Route::post('/pengajuan/PB/edit/editsimpan',[ PerbaikanController::class,'editsimpan'])->middleware('auth');
+Route::get('/pengajuan/PB/hapus/{id}',[PerbaikanController::class,'hapus'])->middleware('auth');
+Route::get('/PB/selesaiPerbaikan/{id}',[PerbaikanController::class,'selesaiPerbaikan'])->middleware('auth');
+Route::post('/PB/selesaiPerbaikan/simpanSelesaiPerbaikan',[ PerbaikanController::class,'simpanSelesaiPerbaikan'])->middleware('auth');
+Route::get('/pengajuan/PB/detail/{id}',[PerbaikanController::class,'detail'])->middleware('auth');
 
 
-Route::post('/jenisBarang/tambah',[ JenisBarangController::class,'store']);
-Route::get('/jenisBarang/hapus/{id}',[ JenisBarangController::class,'hapus']);
+Route::get('/pemutihan',[ PemutihanController::class,'index'])->middleware('auth');
+Route::get('/pemutihan/pilihbarang',[ PemutihanController::class,'pilihbarang'])->middleware('auth');
+Route::get('/pemutihan/pilihbarang/search',[ PemutihanController::class,'pilihbarang'])->middleware('auth');
+Route::get('/pemutihan/pemutihan/{id}',[PemutihanController::class,'pemutihan'])->middleware('auth');
+Route::post('/pemutihan/pemutihan/simpanpemutihan',[PemutihanController::class,'simpanpemutihan'])->middleware('auth');
+Route::get('/pemutihan/detail/{id}',[PemutihanController::class,'detail'])->middleware('auth');
+
+Route::get('/pemutihanLangsung/pilihBarang',[ PemutihanController::class,'pilihbarangPemutihanLangsung'])->middleware('auth');
+Route::get('/pemutihanLangsung/pilihBarang/searchPLangsung',[ PemutihanController::class,'searchPLangsung'])->middleware('auth');
+Route::get('/pemutihan/pemutihanLangung/{id}',[PemutihanController::class,'pemutihanLangsung'])->middleware('auth');
+Route::post('/pemutihan/pemutihanLangsung/simpanpemutihanLangsung',[PemutihanController::class,'simpanpemutihanLangsung'])->middleware('auth');
+
+Route::get('/perawatan',[ PerawatanController::class,'index'])->middleware('auth');
+Route::get('/perawatan/search',[ PerawatanController::class,'search'])->middleware('auth');
+Route::get('/perawatan/pilihBarang',[ PerawatanController::class,'pilihbarangPerawatan'])->middleware('auth');
+Route::get('/perawatan/pilihBarang/search',[ PerawatanController::class,'searchpilihbarangPerawatan'])->middleware('auth');
+Route::get('/perawatan/tambah/{id}',[PerawatanController::class,'perawatan'])->middleware('auth');
+Route::post('/perawatan/simpanperawatan',[PerawatanController::class,'simpanperawatan'])->middleware('auth');
+Route::get('/perawatan/detail/{id}',[PerawatanController::class,'detail'])->middleware('auth');
+Route::get('/perawatan/edit/{id}',[PerawatanController::class,'edit'])->middleware('auth');
+Route::post('/perawatan/edit/editsimpan',[ PerawatanController::class,'editsimpan'])->middleware('auth');
+Route::get('/perawatan/hapus/{id}',[PerawatanController::class,'hapus'])->middleware('auth');
+
+
+Route::get('/ruangan',[ RuanganController::class,'index'])->middleware('auth');
+Route::get('/ruangan/search',[ RuanganController::class,'search'])->middleware('auth');
+Route::get('/ruangan/tambah',[ RuanganController::class,'formTambah'])->middleware('auth');
+Route::post('/ruangan/simpan',[ RuanganController::class,'store'])->middleware('auth');
+Route::get('/ruangan/edit/{id}',[RuanganController::class,'edit'])->middleware('auth');
+Route::post('/ruangan/editsimpan',[ RuanganController::class,'update'])->middleware('auth');
+Route::get('/ruangan/hapus/{id}',[RuanganController::class,'hapus'])->middleware('auth');
+
+
+Route::post('/jenisBarang/tambah',[ JenisBarangController::class,'store'])->middleware('auth');
+Route::get('/jenisBarang/hapus/{id}',[ JenisBarangController::class,'hapus'])->middleware('auth');
 
 
